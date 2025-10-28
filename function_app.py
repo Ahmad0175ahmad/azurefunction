80f01ddab124a820503871b234284ae9bdf9e2ae
Ahmad0175ahmad <ahmadbro175@gmail.com>
2025-10-28 14:19:55 +0500

    Initial commit: Python Azure Function with HTTP trigger

---
+++ function_app.py
@@ -0,0 +1,28 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="HttpTriggerFunction", methods=["GET", "POST"])
+def HttpTriggerFunction(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully.",
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+            "Please pass a name on the query string or in the request body",
+            status_code=400
+        )
